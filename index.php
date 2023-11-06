f7912742a8ca75b5c9fe9e3555204b299a363708
Jhon Francia Minaya <jfranciaminaya@gmail.com>
2023-11-06 02:23:20 -0500

    Se agregó Header y footer PHP

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>CMMS</title>
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
@@ -12,6 +12,7 @@
   <link rel="stylesheet" href="./plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="./dist/css/adminlte.min.css">
+  <link rel="shortcun icon" href="./dist/img/tech-support-24.png">
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
---
+++ views/home/index.php
@@ -0,0 +1,50 @@
+<?php include_once '../header.php'; ?>
+
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Starter Page</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Starter Page</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestiae cupiditate omnis alias praesentium! Voluptatem consectetur similique veniam ipsa accusantium voluptate consequatur? Cupiditate temporibus dolor illum voluptas iure similique est!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestiae cupiditate omnis alias praesentium! Voluptatem consectetur similique veniam ipsa accusantium voluptate consequatur? Cupiditate temporibus dolor illum voluptas iure similique est!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestiae cupiditate omnis alias praesentium! Voluptatem consectetur similique veniam ipsa accusantium voluptate consequatur? Cupiditate temporibus dolor illum voluptas iure similique est!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestiae cupiditate omnis alias praesentium! Voluptatem consectetur similique veniam ipsa accusantium voluptate consequatur? Cupiditate temporibus dolor illum voluptas iure similique est!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestiae cupiditate omnis alias praesentium! Voluptatem consectetur similique veniam ipsa accusantium voluptate consequatur? Cupiditate temporibus dolor illum voluptas iure similique est!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestiae cupiditate omnis alias praesentium! Voluptatem consectetur similique veniam ipsa accusantium voluptate consequatur? Cupiditate temporibus dolor illum voluptas iure similique est!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestiae cupiditate omnis alias praesentium! Voluptatem consectetur similique veniam ipsa accusantium voluptate consequatur? Cupiditate temporibus dolor illum voluptas iure similique est!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestiae cupiditate omnis alias praesentium! Voluptatem consectetur similique veniam ipsa accusantium voluptate consequatur? Cupiditate temporibus dolor illum voluptas iure similique est!
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include_once '../footer.php'; ?>
+
+<!-- Falta invocar mi propio JS -->
+<!-- <script></script> -->
+
+<!-- Fin del HTML -->
+<body>
+</html>
+
