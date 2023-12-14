07dda02b87730db57099da0fdfbb81d8980ac28a
Jhon <jfranciaminaya@gmail.com>
2023-12-14 17:44:18 -0500

    Asignación primer avance

--- views/asignacion/index.php
+++ views/asignacion/index.php
@@ -19,22 +19,85 @@
   </div>
   <!-- /.content-header -->
 
+  <style>
+    tr th { text-align: center; }
+    tr td:nth-child(1){ text-align: center; }
+    tr td:nth-child(3){ text-align: center; }
+    tr td:nth-child(4){ text-align: center; }
+    .form-label { font-weight: normal !important; }
+  </style>
+
   <div class="content">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-md-12">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestiae cupiditate omnis alias praesentium! Voluptatem consectetur similique veniam ipsa accusantium voluptate consequatur? Cupiditate temporibus dolor illum voluptas iure similique est!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestiae cupiditate omnis alias praesentium! Voluptatem consectetur similique veniam ipsa accusantium voluptate consequatur? Cupiditate temporibus dolor illum voluptas iure similique est!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestiae cupiditate omnis alias praesentium! Voluptatem consectetur similique veniam ipsa accusantium voluptate consequatur? Cupiditate temporibus dolor illum voluptas iure similique est!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestiae cupiditate omnis alias praesentium! Voluptatem consectetur similique veniam ipsa accusantium voluptate consequatur? Cupiditate temporibus dolor illum voluptas iure similique est!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestiae cupiditate omnis alias praesentium! Voluptatem consectetur similique veniam ipsa accusantium voluptate consequatur? Cupiditate temporibus dolor illum voluptas iure similique est!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestiae cupiditate omnis alias praesentium! Voluptatem consectetur similique veniam ipsa accusantium voluptate consequatur? Cupiditate temporibus dolor illum voluptas iure similique est!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestiae cupiditate omnis alias praesentium! Voluptatem consectetur similique veniam ipsa accusantium voluptate consequatur? Cupiditate temporibus dolor illum voluptas iure similique est!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio molestiae cupiditate omnis alias praesentium! Voluptatem consectetur similique veniam ipsa accusantium voluptate consequatur? Cupiditate temporibus dolor illum voluptas iure similique est!
-          </p>
+        <div class="col-md-6">
+          <table class="table table-sm table-striped table-bordered">
+            <colgroup>
+              <col width="5%">
+              <col width="65%">
+              <col width="15%">
+              <col width="15%">
+            </colgroup>
+            <thead>
+              <tr class="table-primary">
+                <th>#</th>
+                <th>Descripción del activo</th>
+                <th>Stock</th>
+                <th>Asignación</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Computadoras AIO Dell</td>
+                <td><input type="number" min="0" max="5" class="form-control form-control-sm text-right"></td>
+                <td><input type="number" min="0" max="5" class="form-control form-control-sm text-right"></td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Computadoras AIO Dell</td>
+                <td><input type="number" min="0" max="5" class="form-control form-control-sm text-right"></td>
+                <td><input type="number" min="0" max="5" class="form-control form-control-sm text-right"></td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Computadoras AIO Dell</td>
+                <td><input type="number" min="0" max="5" class="form-control form-control-sm text-right"></td>
+                <td><input type="number" min="0" max="5" class="form-control form-control-sm text-right"></td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Computadoras AIO Dell</td>
+                <td><input type="number" min="0" max="5" class="form-control form-control-sm text-right"></td>
+                <td><input type="number" min="0" max="5" class="form-control form-control-sm text-right"></td>
+              </tr>
+            </tbody>
+          </table>
+        </div> <!-- ./col-md-6 -->
+
+        <div class="col-md-6">
+          <ul class="list-group">
+            <li class="list-group-item active">Áreas de asignación</li>
+            <li class="list-group-item">
+              <div class="form-group">
+                <label for="">Áreas disponibles</label>
+                <select name="" id="" class="form-control form-control-sm">
+                  <option value="">Seleccione</option>
+                </select>
+              </div>
+              <ul>
+                <li>Activo 1 <a href="#">Quitar</a></li>
+                <li>Activo 1 <a href="#">Quitar</a></li>
+                <li>Activo 1 <a href="#">Quitar</a></li>
+                <li>Activo 1 <a href="#">Quitar</a></li>
+                <li>Activo 1 <a href="#">Quitar</a></li>
+              </ul>
+            </li>
+          </ul>
         </div>
-      </div>
+
+      </div> <!-- ./row -->
+
     </div>
   </div>
 </div>
